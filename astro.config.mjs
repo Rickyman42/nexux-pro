a9649903ff65444c3f4fc6369c877caec0441872
Rickyman42 <info@nexux.es>
2026-05-17 17:22:16 +0200

    fix: output:server, hide WA for starter, correct bot username in portal

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://nexux.pro',
+  output: 'server',
   adapter: vercel(),
   integrations: [sitemap({
     filter: (page) => !page.includes('/gracias'),
